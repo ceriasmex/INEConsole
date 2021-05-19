66d6669b3c5d3053ae62ad0b6fd5b934475143a4
Julio Alberto Carrillo <ceriasmex@gmail.com>
2021-05-19 16:51:42 -0500

    With count all kind off containers

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ def validatestatus():
 
     incodef="incodetech/ms-incodesmile-recognition-service:cpu"
     incodeinew="incodetech/incode-ine-gateway"
+    incodeb="incodetech/nginx-loadbalancer"
     incodefc=0
     incodeinewc=0
+    incodebc=0
 
 
     client=docker.from_env()
@@ -31,8 +33,10 @@ def validatestatus():
            incodefc+=1
         elif runc.find(incodeinew) != -1:
            incodeinewc+=1
+        elif runc.find(incodeb) != -1:
+           incodeinbc+=1
 
-    alertinc="Contenedores de incodesmile corriendo: " + str(incodefc) + "\nContenedores de incode ine gatewau correindo: " + str(incodeinewc)
+    alertinc="Contenedores de incodesmile corriendo: " + str(incodefc) + "\nContenedores de incode ine gateway correindo: " + str(incodeinewc) + "\nContenedores de incode loadbalancer: " + str(incodebc)
     print(alertinc)
 
     PROCNAME={"docker","podman","java"}
