af2987a8c4aa2511ae2a5b4256fa60ca17408bc1
Julio Alberto Carrillo <ceriasmex@gmail.com>
2021-05-19 12:06:59 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import platform
+
+
+def serverdata():
+    import platform
+    hostname=platform.node()
+    return(hostname)
+
+def print_menu(hostname):
+    import platform
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Consola de validación de servicios INCODE en el INE, Nodo - {hostname}\n')
+    print("El menu tiene la finalida de facilitar el soporte remoto de los servidores de comparación de incode\n")
+    print("Opciones:\n")
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    host=serverdata()
+    print_menu(host)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
