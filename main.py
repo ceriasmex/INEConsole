4853c9e4267fd5c3a5b088c0c61683ed7f3903cf
Julio Alberto Carrillo <ceriasmex@gmail.com>
2021-05-19 19:17:10 -0500

    With count all kind off containers

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def validatestatusc():
         alertfc=alertfc + "\t\t\tEs el numero correcto de contenedores."
         print(Style.BRIGHT + Back.GREEN + Fore.WHITE + alertfc)
     else:
-        alertfc = alertfc + "\t\t\t\tNO es el numero correo de contenedores."
+        alertfc = alertfc + "\t\t\tNO es el numero correo de contenedores."
         print(Style.BRIGHT + Back.RED + Fore.WHITE + alertfc)
 
     if incodeinewc == 1:
