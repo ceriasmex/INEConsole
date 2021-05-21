21b64a7cbee22dddf26275430c05b7342c0b9041
Julio Alberto Carrillo <ceriasmex@gmail.com>
2021-05-20 20:39:12 -0500

    With count all kind off containers

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ def validatestatusc():
             break
         service,data=line.split("=")
         service=str(service).strip()
+        data = str(data)
         if service.find('INEGW') != -1:
-            data=str(data)
             inewp=ping(data,count=10)
             if inewp.rtt_avg_ms < 0.1:
                 gwine="Se tiene acceso al GW de la red INE "+data
